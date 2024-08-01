fba38ddc43bb922985736eab275dd8b892f57e8c
Vali <78507032+Vali0004@users.noreply.github.com>
2024-08-01 14:43:04 -0400

    Going back to manual pointer management. Oops

--- dawn/src/thread_manager/thread_manager.h
+++ dawn/src/thread_manager/thread_manager.h
@@ -4,108 +4,108 @@
 
 namespace dwn
 {
-	class thread_manager
-	{
-	public:
-		thread_manager()
-		{}
-		~thread_manager()
-		{
-			for (auto& thr : m_threads)
-			{
-				thr.release();
-			}
-			m_threads.clear();
-		}
+    class thread_manager
+    {
+    public:
+        thread_manager()
+        {}
+        ~thread_manager()
+        {
+            for (auto& thr : m_threads)
+            {
+                delete thr;
+            }
+            m_threads.clear();
+        }
 
-		nodisc void add_task(std::function<void()> task)
-		{
-			if (task)
-			{
-				m_threads.push_back(std::unique_ptr<thread>(create_thread_wrapper(task)));
-				auto& t{ m_threads.back() };
-				t->start();
-			}
-		}
+        nodisc void add_task(std::function<void()> task)
+        {
+            if (task)
+            {
+                m_threads.push_back(create_thread_wrapper(task));
+                thread* t{ m_threads.back() };
+                t->start();
+            }
+        }
 
-		nodisc void update_thread_status()
-		{
-			for (u64 i{}; i != m_threads.size(); ++i)
-			{
-				auto& thr{ m_threads[i] };
-				if (thr && !thr->get_id())
-				{
-					thr.release();
-					m_threads.erase(m_threads.begin() + i);
-					break;
-				}
-			}
-		}
+        nodisc void update_thread_status()
+        {
+            for (u64 i{}; i != m_threads.size(); ++i)
+            {
+                auto& thr{ m_threads[i] };
+                if (thr && !thr->get_id())
+                {
+                    delete thr;
+                    m_threads.erase(m_threads.begin() + i);
+                    break;
+                }
+            }
+        }
 
-		auto create_thread(std::function<void(thread*)> callback)
-		{
-			thread* thr{ create_thread_wrapper(callback) };
-			m_threads.push_back(std::unique_ptr<thread>(thr));
-			auto& ret{ m_threads.back() };
-			ret->start();
-			return ret.get();
-		}
+        thread* create_thread(std::function<void(thread*)> callback)
+        {
+            thread* thr{ create_thread_wrapper(callback) };
+            m_threads.push_back(thr);
+            thread* ret{ m_threads.back() };
+            ret->start();
+            return ret;
+        }
 
-		auto create_thread(std::function<void()> callback)
-		{
-			thread* thr{ create_thread_wrapper(callback) };
-			m_threads.push_back(std::unique_ptr<thread>(thr));
-			auto& ret{ m_threads.back() };
-			ret->start();
-			return ret.get();
-		}
+        thread* create_thread(std::function<void()> callback)
+        {
+            thread* t{ create_thread_wrapper(callback) };
+            m_threads.push_back(t);
+            thread* ret{ m_threads.back() };
+            ret->start();
+            return ret;
+        }
 
-		const bool empty() noexcept
-		{
-			return m_threads.empty();
-		}
+        const bool empty() noexcept
+        {
+            return m_threads.empty();
+        }
 
-	private:
-		nodisc thread* create_thread_wrapper(std::function<void()> callback)
-		{
-			if (!callback)
-			{
-				return nullptr;
-			}
-			auto thr = std::make_unique<thread>([](void* a) -> u32 {
-				std::function<void()>* callback_ptr{ reinterpret_cast<decltype(callback_ptr)>(a) };
-				if (callback_ptr && *callback_ptr)
-				{
-					(*callback_ptr)();
-				}
-				return 0;
-			}, &callback);
-			return thr.get();
-		}
+    private:
+        nodisc thread* create_thread_wrapper(std::function<void()> callback)
+        {
+            if (!callback)
+            {
+                return nullptr;
+            }
+            thread* thr = new thread([](void* cb) -> u32 {
+                std::function<void()>* callback_ptr{ reinterpret_cast<decltype(callback_ptr)>(cb) };
+                if (callback_ptr && *callback_ptr)
+                {
+                    (*callback_ptr)();
+                }
+                return 0;
+            }, &callback);
+            return thr;
+        }
 
-		nodisc thread* create_thread_wrapper(std::function<void(thread*)> callback)
-		{
-			if (!callback)
-			{
-				return nullptr;
-			}
-			args<2> thread_args{};
-			thread_args.set_arg(0, &callback);
-			std::unique_ptr<thread> thr{};
-			thread_args.set_arg(1, thr.get());
-			thr = std::make_unique<thread>([](void* a) -> u32 {
-				args<2>* args{ reinterpret_cast<decltype(args)>(a) };
-				std::function<void(thread*)>* function{ reinterpret_cast<decltype(function)>(args->get_arg(0)) };
-				thread* _this{ reinterpret_cast<decltype(_this)>(args->get_arg(1)) };
-				(*function)(_this);
-				return 0;
-			}, &thread_args);
-			thread_args.set_arg(1, thr.get());
-			return thr.get();
-		}
+        nodisc thread* create_thread_wrapper(std::function<void(thread*)> callback)
+        {
+            if (!callback)
+            {
+                return nullptr;
+            }
+            args<2> thread_args{};
+            thread_args.set_arg(0, &callback);
+            thread* thr{};
+            thread_args.set_arg(1, thr);
+            thr = new thread([](void* a) -> u32 {
+                args<2>* args{ reinterpret_cast<decltype(args)>(a) };
+                std::function<void(thread*)>* function{ reinterpret_cast<decltype(function)>(args->get_arg(0)) };
+                thread* _this{ reinterpret_cast<decltype(_this)>(args->get_arg(1)) };
+                (*function)(_this);
+                return 0;
+            }, &thread_args);
+            thread_args.set_arg(1, thr);
+            return thr;
+        }
 
-		std::vector<std::unique_ptr<thread>> m_threads{};
-	};
+        std::vector<thread*> m_threads{};
+    };
 
-	inline thread_manager g_thread_manager{};
+    inline thread_manager g_thread_manager{};
 }
